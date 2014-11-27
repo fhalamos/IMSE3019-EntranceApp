119573f35241395db8884528b96b84cc0d330164
Felipe Alamos <felipealamos@gmail.com>
2014-11-28 01:41:56 +0800

    Change assembly name

--- EntranceApp/Properties/Settings.Designer.cs
+++ EntranceApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace imseWCard2.Properties {
+namespace EntranceApp.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
